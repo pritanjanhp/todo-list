91171a9ef10c8da626682fb81252ff7bd2e246ea
pritanjanhp <pritanjanbaranwal03.hp@gmail.com>
2025-01-02 11:58:10 +0530

    done

--- src/componets/TodoList.jsx
+++ src/componets/TodoList.jsx
@@ -8,21 +8,23 @@ const TodoList = ({ todos, toggleFinish, handleDelete, handleEdit, updateData, s
         <div className='todo-list'>
             {
                 todos.length !== 0 ? todos.map((item, idx) => (
-                    updateData.id === item.id ? <form className='todo-item' onSubmit={() => handleUpdate(updateData.id)}>
-                        <input type='text' placeholder='update' value={updateData.text} onChange={(e) => setUpdateData({ ...updateData, text: e.target.value })} />
-                        <button type='submit' className='todo-button-add'>Update</button>
-                    </form> : <div className='todo-item' key={item.id}>
-                        <input
-                            type="checkbox"
-                            finish={item.finish}
-                            onChange={() => toggleFinish(item.id)}
-                        />
-                        <span className='todo-text'>
-                            {item.text}
-                        </span>
-                        <button className='todo-button-delete' onClick={() => handleDelete(idx)}><MdAutoDelete /></button>
-                        <button className='todo-button-edit' onClick={() => setUpdateData({ id: item.id, text: item.text, finish: item.finish })}><CiEdit /></button>
-                    </div>
+                    updateData.id === item.id ?
+                        <form className='todo-item' onSubmit={() => handleUpdate(updateData.id)}>
+                            <input type='text' placeholder='update' value={updateData.text} onChange={(e) => setUpdateData({ ...updateData, text: e.target.value })} className='todo-edit' />
+                            <button type='submit' className='todo-button-add'>Update</button>
+                        </form> : <div className='todo-item' key={item.id}>
+                            <input
+                                type="checkbox"
+                                finish={item.finish}
+                                onChange={() => toggleFinish(item.id)}
+                            />
+                            <span className='todo-text' style={{ color: item.finish ? '#979393' : 'black', textDecoration: item.finish ? 'line-through' : 'none' }}
+                            >
+                                {item.text}
+                            </span>
+                            <button className='todo-button-delete' onClick={() => handleDelete(idx)}><MdAutoDelete /></button>
+                            <button className='todo-button-edit' onClick={() => setUpdateData({ id: item.id, text: item.text, finish: item.finish })}><CiEdit /></button>
+                        </div>
                 )) : <h4> No data is entered</h4>
             }
         </div>
